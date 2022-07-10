b7119f162beaeba7ed93148661be61f9961455fa
M-Topcu <mamitopcu35@gmail.com>
2022-07-10 21:36:12 +0200

    refaktor

--- Deneme/src/MDArraysProblems/Problem4.java
+++ Deneme/src/MDArraysProblems/Problem4.java
@@ -3,31 +3,37 @@ package MDArraysProblems;
 import java.util.Arrays;
 import java.util.Scanner;
 
+import static Arrays.MethodsArray.arrayOlustur;
+
 public class Problem4 {
     // Alinan bir arrayde istenen bir degere esit olan elemanlari kaldirip kalanlari yeni bir array yapalim
 
     public static void main(String[] args) {
         Scanner scan  = new Scanner(System.in);
 
-        int son[] = new int[1];
+        int arr[] =arrayOlustur();
+        System.out.println("Array`iniz: " +Arrays.toString(arr));
 
-
-        System.out.println(Arrays.toString(son));
 
         System.out.println("Kaldirilacak elemani giriniz");
         int kaldirilacak = scan.nextInt();
-        String arrS = "";
+        String str = "";
 
-        for (int i = 0; i <= son.length-1 ; i++) {
-            if (son[i] != kaldirilacak){
-                arrS+=son[i];
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] != kaldirilacak) {
+                str+= arr[i] + " ";
             }
         }
-        int arrSon[] = new int[arrS.length()];
-        for (int i = 0; i < arrS.length(); i++) {
 
+        String str2[] = str.trim().split(" ");
+        int son[] = new int[str2.length];
+
+        for (int i = 0; i <=str2.length-1 ; i++) {
+            son[i] =(int) Double.parseDouble(str2[i]);
         }
-        System.out.println(Arrays.toString(arrSon));
+        System.out.println(kaldirilacak+ " olmadan araay: " +Arrays.toString(son));
+
+
 
 
     }
