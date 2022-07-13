03db59c9591f0735ad5d2dbbbd8ccdd9ee1ff7b2
M-Topcu <mamitopcu35@gmail.com>
2022-07-13 16:40:30 +0200

    ConsCall

---
+++ Deneme/src/Constructor/ConstructorCall.java
@@ -0,0 +1,19 @@
+package Constructor;
+
+public class ConstructorCall {
+
+    int x = 5;
+    ConstructorCall(){
+        System.out.println("-x" + x);
+    }
+    ConstructorCall(int x) {
+        this(); // cons cagirdi, parametrelere gore - ilk sayira yazilmak zorunda
+        System.out.println("-x" + x);
+    }
+
+    public static void main(String[] args) {
+        ConstructorCall mc1 = new ConstructorCall(4);
+
+        ConstructorCall mc2 = new ConstructorCall();
+    }
+}
