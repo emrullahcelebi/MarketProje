c78b4b1e73358f07c852c8e49d069d8530e2d51e
M-Topcu <mamitopcu35@gmail.com>
2022-07-23 23:06:41 +0200

    Practicestart

---
+++ javaPracticeFall/src/day8_practice/Q03_ArrayList.java
@@ -0,0 +1,19 @@
+package day8_practice;
+
+import java.util.Arrays;
+
+public class Q03_ArrayList {
+
+    /*
+     * verilen arrayin n sayisi kadar maksimun  degerlerini bulan java programi yaziniz
+     * not kisa metodlar kullamayiniz
+     * Input : {1,2,3,4,5,6,7,8} input n:2 Output : 7 and 8
+     */
+    public static void main(String[] args) {
+
+        int input [] = {1,2,3,4,5,6,7,8};
+        int inpt = 2;
+
+
+    }
+}
---
+++ javaPracticeFall/src/day8_practice/Q09_SurviveMonkey.java
@@ -0,0 +1,14 @@
+package day8_practice;
+
+public class Q09_SurviveMonkey {
+    /*
+		There is a lonely monkey in the island
+		He needs to eat 4 bananas every day
+		there are just 165 bananas in that island
+		Create following variables and find how many days
+		monkey can survive.
+		Use do while loop, increment and decrement and if statements
+		int numberOfBananas =165, survivalDays = 1;
+		boolean monkeyAlive = true;
+		*/
+}
