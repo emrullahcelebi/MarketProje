efdc9c249461d8e5c85cfdd2d190d454f36fe3e0
M-Topcu <mamitopcu35@gmail.com>
2022-07-24 22:23:58 +0200

    sirala

--- javaPracticeFall/src/day8_practice/Q03_ArrayList.java
+++ javaPracticeFall/src/day8_practice/Q03_ArrayList.java
@@ -1,6 +1,8 @@
 package day8_practice;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 public class Q03_ArrayList {
 
@@ -13,6 +15,28 @@ public class Q03_ArrayList {
 
         int input [] = {1,2,3,4,5,6,7,8};
         int inpt = 2;
+
+        List<Integer> list = new ArrayList<Integer>();
+        int kucuk = input[0];
+        int index = 0;
+
+        for (int i = 0; i < input.length; i++) {
+            for (int j = 0; j < input.length; j++) {
+                if (input[j]<=kucuk){
+                    kucuk = input[j];
+                    index = j;
+                }
+            }
+            list.add(kucuk);
+            kucuk= Integer.MAX_VALUE;
+            input[index] = Integer.MAX_VALUE;
+
+        }
+        System.out.println(list);
+
+        for (int i = list.size() - 1; i >= list.size()-inpt; i--) {
+            System.out.print(list.get(i) + " ");
+        }
 
 
     }
